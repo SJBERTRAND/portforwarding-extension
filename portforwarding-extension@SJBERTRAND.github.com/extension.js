ed2e1bf8b378c69fc0c6bb61cb473b07c034c26e
SJBERTRAND <sylvain.j.bertrand@gmail.com>
2024-08-25 19:10:54 -0600

    Version 3- Multi connections and multiport

--- portforwarding-extension@SJBERTRAND.github.com/extension.js
+++ portforwarding-extension@SJBERTRAND.github.com/extension.js
@@ -31,97 +31,185 @@ import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
 
 
+const SSHServerConnection = class {
+    constructor() {
+
+    };
+    
+    
+    _createSwitches(_settings,_menu,_number,_icon,_cancellable){
+            
+        //When switches are created set all server visible value to false
+        _settings.set_boolean('connection-active'+_number, false);
+        
+       // Connect to value changes and update icons
+       _settings.connect('changed::connection-active'+_number, ()=> {
+        
+                
+                if (_settings.get_boolean('connection-active1') || _settings.get_boolean('connection-active2') || _settings.get_boolean('connection-active3') || _settings.get_boolean('connection-active4') ){
+                    _icon.icon_name = 'network-transmit-receive-symbolic';
+                }else{
+                    _icon.icon_name = 'network-offline-symbolic';
+                };      
+       });       
+       
+        const _switch = this._buildSwitch(_settings,_number,_cancellable);
+        _menu.addMenuItem(_switch,_number);
+              
+    }; // end of Create switch
+    
+    
+    
+    _buildSwitch(_settings,_number,_cancellable){
+
+        //Create the switch
+        const _switch =  new PopupMenu.PopupSwitchMenuItem(_(_settings.get_string('server-name'+_number)), false);
+        
+        // Bind the value of connection-name to the ConnectionSwitch label text value
+        _settings.bind('server-name'+_number, _switch.label, 'text', Gio.SettingsBindFlags.DEFAULT);
+        
+        // Create a variable for the subprocess
+        var _subprocess = null;
+        
+        // Create the switch signal that build and execute the command when switch to on
+        _switch.connect('toggled', () =>{
+        
+            if ( _switch.state == true){
+            
+                const _command = this._buildCommand(_settings,_number);
+                
+                // Assign Gio.Subprocess to the _subprocess of the switch
+                _subprocess = Gio.Subprocess.new(_command, Gio.SubprocessFlags.STDOUT_SILENCE);
+                
+                // Create a subprocess status value with a default of 0
+                _subprocess.status = 0;
+                
+                // Connect to the value status and monitor if to changes to 1 and when it does change the switch
+                _subprocess.connect('notify::status', () => {
+                    if ( _subprocess.status == 1 ){
+                        _switch.setToggleState(false)
+                    };
+                });
+                            
+                // Initialized the SSH connection and call _subprocessCompleted when it fails or get terminated
+                _subprocess.wait_async(_cancellable,this._subprocessCompleted);
+                
+                // Change the value to change the icon
+                _settings.set_boolean('connection-active'+_number, true);
+                
+            
+            }; //End if statement for switch state true
+            
+            if ( _switch.state == false){
+            
+                if ( _subprocess != null ) {
+                    _subprocess.force_exit();
+                    _subprocess = null;
+                };
+                _settings.set_boolean('connection-active'+_number, false);
+                            
+            }; //End if statement for switch state false
+            
+        
+
+        }); //End of switch signal        
+        return _switch;
+    }; //End of build switch
+    
+    
+    
+    _buildCommand(_settings,_number){
+    
+    const CommandArray = [];
+        if (_settings.get_boolean('password-required'+_number) == true) {
+            CommandArray.push("sshpass", "-p", _settings.get_string('server-password'+_number), "ssh" ,"-N");
+        }else{
+            CommandArray.push("ssh" ,"-N", "-o", "PasswordAuthentication=no");
+        };
+
+        // Create an array of all the ports
+        const regex = /[0-9]*[0-9]/g;
+        const RemotePorts = _settings.get_string('server-port'+_number).match(regex);
+        const HostPorts = _settings.get_string('host-port'+_number).match(regex);
+
+        RemotePorts.forEach(ports => {
+            // Find position of item in first array
+            var Position = RemotePorts.indexOf(ports);
+            //Look that there is a second port otherwise use the first one
+                if ( HostPorts.at(Position) == undefined ) {
+                    CommandArray.push("-L", ports+":"+_settings.get_string('server-address'+_number)+":"+ports );
+                }else {
+                    CommandArray.push("-L", HostPorts.at(Position)+":"+_settings.get_string('server-address'+_number)+":"+ports );
+                };
+        });
+
+        CommandArray.push( _settings.get_string('server-login'+_number)+"@"+_settings.get_string('server-address'+_number));
+
+        return CommandArray
+    }; //End of build command
+
+
+    _subprocessCompleted(_subprocess, _result){
+        _subprocess.status = 1;
+        if ( _subprocess != null ) {
+            _subprocess.force_exit();
+            _subprocess = null;
+        };       
+    }; //End of _subprocessCompleted
+
+
+}; // End of constructor
+
+
+
+
 const Indicator = GObject.registerClass(
 class Indicator extends PanelMenu.Button {
-    _init(_settings) {
+    _init(_settings,_sshForwarding) {
         super._init(0.0, _('SSH Port Forwarding Extension'));
         
-        const _icon_on = 'network-transmit-receive-symbolic';
-        const _icon_off = 'network-offline-symbolic';
+        this._settings = _settings;
+        //this._sshForwarding = _sshForwarding; // Not sure if I should keep that one
         
-        let _icon = new St.Icon({
-            icon_name: _icon_off,
+        
+        this._icon = new St.Icon({
+            icon_name: 'network-offline-symbolic',
             style_class: 'system-status-icon',
             });
+        this.add_child(this._icon);
         
-        this.add_child(_icon);
-
-        // Create new switch item
-        let ConnectionSwitch = new PopupMenu.PopupSwitchMenuItem(_(_settings.get_string('connection-name')), false);
-        // Bind the value of connection-name to the ConnectionSwitch label text value
-        _settings.bind('connection-name', ConnectionSwitch.label, 'text', Gio.SettingsBindFlags.DEFAULT);
+        this._cancellable = new Gio.Cancellable();
         
-        // Create the _SubProcess property
-        this._SubProcess = null;
-        
-        //Connect to the switch and active the connection when on and deactivate when off
-        ConnectionSwitch.connect('toggled' , () =>{
-            if (ConnectionSwitch.state == true){
-            // update the icon
-            _icon.icon_name = _icon_on;
-            
-            //Create a function to verify if the subprocess has succeded
-            function ProcCompleted(){
-                ConnectionSwitch.setToggleState(false);
-                _icon.icon_name = _icon_off;
-                };
-                    
-            //Create a subprocess to Call SSH based on if password is required
-            this._Command = null;
-            
-            if( _settings.get_boolean('pass-required') ){
-            this._Command = [ "/usr/bin/sshpass", "-p" , _settings.get_string('remote-password') , "ssh" ,"-N" ,"-L",_settings.get_string('host-port')+":"+_settings.get_string('host-address')+":"+_settings.get_string('remote-port') , _settings.get_string('remote-login')+"@"+_settings.get_string('remote-address') ];
-            }else{
-            this._Command = [ "/usr/bin/ssh" ,"-N", "-o", "PasswordAuthentication=no","-L", _settings.get_string('host-port')+":"+_settings.get_string('host-address')+":"+_settings.get_string('remote-port') , _settings.get_string('remote-login')+"@"+_settings.get_string('remote-address') ];
-            };
-            
-            
-            // Attach the subprocess to the indicator with a new properties/value of _SubProcess 
-            this._SubProcess = Gio.Subprocess.new(this._Command, Gio.SubprocessFlags.NONE); // When finish switch to STDOUT_SILENCE
-            //const proc = this._SubProcess;
-            const cancellable = new Gio.Cancellable();
-            this._SubProcess.wait_async(cancellable,ProcCompleted);
-
-            }; // End of if statement if true
-            
-            if (ConnectionSwitch.state == false){
-            //update icon
-            _icon.icon_name = _icon_off;
-                    if ( this._SubProcess != null){
-                        this._SubProcess.force_exit();
-                        this._SubProcess = null;
-                        };
-            };
-        });
-        
-         
-        //Add the switch to the menu
-        this.menu.addMenuItem(ConnectionSwitch);
+        _sshForwarding._createSwitches(this._settings,this.menu,1,this._icon,this._cancellable);
+        _sshForwarding._createSwitches(this._settings,this.menu,2,this._icon,this._cancellable);
+        _sshForwarding._createSwitches(this._settings,this.menu,3,this._icon,this._cancellable);
+        _sshForwarding._createSwitches(this._settings,this.menu,4,this._icon,this._cancellable);
+        _sshForwarding._createSwitches(this._settings,this.menu,5,this._icon,this._cancellable);
 
     }
 });
+
 
 export default class SSHPortForwardingExtension extends Extension {
     enable() {
         //Get the settings saved from the preferences
         this._settings = this.getSettings();
-        // Create the indicator and passed the settings to it so it can be used while creating widget inside the indicator
-        this._indicator = new Indicator(this._settings);
+        this._sshForwarding = new SSHServerConnection(this._cancellable);
+        this._indicator = new Indicator(this._settings, this._sshForwarding);
         Main.panel.addToStatusArea(this.uuid, this._indicator);
         
-         // Add a menu item to open the preferences window
-        this._indicator.menu.addAction(_('Connection Settings'),
-            () => this.openPreferences());
-        
+        // Add a menu item to open the preferences window
+        this._indicator.menu.addAction(_('Connection Settings',6),
+            () => this.openPreferences());    
     }
 
     disable() {
-        if ( this._indicator._SubProcess != null ){
-            this._indicator._SubProcess.force_exit();
-            this._indicator._SubProcess = null;
-        };
+        // Cancel all subprocess
+        this._indicator._cancellable.cancel();        
+        this._sshForwarding = null;
         this._indicator.destroy();
         this._indicator = null;
         this._settings = null;
     }
 }
+
--- portforwarding-extension@SJBERTRAND.github.com/prefs.js
+++ portforwarding-extension@SJBERTRAND.github.com/prefs.js
@@ -4,95 +4,95 @@ import GLib from 'gi://GLib';
 
 import {ExtensionPreferences, gettext as _} from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
 
-
-export default class SSHPortForwardingPreferences extends ExtensionPreferences {
-    fillPreferencesWindow(window) {
-        // Create a preferences page, with a single group
+// Function to create each connection pages
+function CreateConnection(window,number){
         const page = new Adw.PreferencesPage({
-            title: _('General'),
-            icon_name: 'dialog-information-symbolic',
+            title: _('Server '+number),
+            icon_name: _('security-high'), 
         });
-        window.add(page);
-
+        window.add(page)
+        
+        // SSH option Group
         const group = new Adw.PreferencesGroup({
-            title: _('Port Forwarding through SSH connection'),
-            description: _('On New Connection connect using SSH in terminal'),
+            title: _('Server '+number+' settings'),
         });
         page.add(group);
         
-        //Create and new entry row for the connection name
-        const connection_name = new Adw.EntryRow({
-            title: _('Enter Connection Name'),
-            show_apply_button : true,
+        //Create a server name 
+        const ServerNameRow = new Adw.EntryRow({
+            title: _('Server Name'),
         });
-        group.add(connection_name);
-        
-        //Create and new entry row for host port
-        const host_port_row = new Adw.EntryRow({
-            title: _('Enter host port'),
-            show_apply_button : true,
-        });
-        group.add(host_port_row);
-        
-        
-        //Create and new entry row for host name
-        const host_address_row = new Adw.EntryRow({
-            title: _('Enter host name/IP'),
-            show_apply_button : true,
-        });
-        group.add(host_address_row);
-        
-        //Create and new entry row for remote port
-        const remote_port_row = new Adw.EntryRow({
-            title: _('Enter remote port'),
-            show_apply_button : true,
-        });
-        group.add(remote_port_row);
-        
-         //Create and new entry row for remote address
-        const remote_address_row = new Adw.EntryRow({
-            title: _('Enter remote address/ip'),
-            show_apply_button : true,
-        });
-        group.add(remote_address_row);
+        group.add(ServerNameRow);
         
         //Create and new entry row for remote address
-        const remote_login_name = new Adw.EntryRow({
-            title: _('Enter remote login name'),
-            show_apply_button : true,
+        const RemoteAddressRow = new Adw.EntryRow({
+            title: _('Enter remote server address/ip'),
         });
-        group.add(remote_login_name);
+        group.add(RemoteAddressRow);
         
-        // Create password required row
-        
-        const pass_switch = new Adw.SwitchRow({
-            title: "Use Password instead of key file"
+        //Create a row for remote port
+        const RemotePortRow = new Adw.EntryRow({
+            title: _('Enter remote server port(s)'),
         });
-        group.add(pass_switch);
+        group.add(RemotePortRow);
+        
+        // Create an action row
+        const InfoRow = new Adw.ActionRow({
+            title: _('For multiple ports separate the port numbers with a space'),
+            subtitle: _('The first remote port will bind with the first host port and so on'),
+        });
+        group.add(InfoRow);
+        
+        //Create a row for host port
+        const HostPortRow = new Adw.EntryRow({
+            title: _('Enter host server port(s)'),
+        });
+        group.add(HostPortRow);
+        
+        //Create a login row
+        const RemoteUserNameRow = new Adw.EntryRow({
+            title: _('Enter remote server username'),
+        });
+        group.add(RemoteUserNameRow);
+        
+        //Create a password required switch
+        const PasswordSwitch = new Adw.SwitchRow({
+            title: _('Use password instead of key file'),
+        });
+        group.add(PasswordSwitch);
         
         // Create password row
-        const pass_row = new Adw.PasswordEntryRow({
-            title: _('Enter remote password '),
+        const PasswordRow = new Adw.PasswordEntryRow({
+            title: _('Enter remote server password '),
         });
-        group.add(pass_row);
+        group.add(PasswordRow);
+    
         
-        // Create a settings object and bind the host_port_row to the `host-port` key
+        // Add the settings for the page here
+        //window._settings.bind('server-visible'+number, ShowServerSwitch, 'active', Gio.SettingsBindFlags.DEFAULT);
+        window._settings.bind('server-name'+number, ServerNameRow, 'text', Gio.SettingsBindFlags.DEFAULT);
+        window._settings.bind('server-address'+number, RemoteAddressRow, 'text', Gio.SettingsBindFlags.DEFAULT);
+        window._settings.bind('server-port'+number, RemotePortRow, 'text', Gio.SettingsBindFlags.DEFAULT);
+        window._settings.bind('host-port'+number, HostPortRow, 'text', Gio.SettingsBindFlags.DEFAULT);
+        window._settings.bind('server-login'+number, RemoteUserNameRow, 'text', Gio.SettingsBindFlags.DEFAULT);
+        window._settings.bind('password-required'+number, PasswordSwitch, 'active', Gio.SettingsBindFlags.DEFAULT);
+        window._settings.bind('server-password'+number, PasswordRow, 'text', Gio.SettingsBindFlags.DEFAULT);
+};
+//End of function
+
+
+export default class SSHPortForwardingPreferences extends ExtensionPreferences {
+    fillPreferencesWindow(window) {
+    
+    
+     window.set_resizable(false);        
+        // Import settings
         window._settings = this.getSettings();
-        window._settings.bind('connection-name', connection_name, 'text',
-            Gio.SettingsBindFlags.DEFAULT);
-        window._settings.bind('host-port', host_port_row, 'text',
-            Gio.SettingsBindFlags.DEFAULT);
-        window._settings.bind('host-address', host_address_row, 'text',
-            Gio.SettingsBindFlags.DEFAULT);
-        window._settings.bind('remote-port', remote_port_row, 'text',
-            Gio.SettingsBindFlags.DEFAULT);
-        window._settings.bind('remote-address', remote_address_row, 'text',
-            Gio.SettingsBindFlags.DEFAULT);
-        window._settings.bind('remote-login', remote_login_name, 'text',
-            Gio.SettingsBindFlags.DEFAULT);
-        window._settings.bind('remote-password', pass_row, 'text',
-            Gio.SettingsBindFlags.DEFAULT);
-        window._settings.bind('pass-required', pass_switch, 'active',
-            Gio.SettingsBindFlags.DEFAULT);
+
+        CreateConnection(window,'1');
+        CreateConnection(window,'2');
+        CreateConnection(window,'3');
+        CreateConnection(window,'4');
+        CreateConnection(window,'5');
     }
 }
